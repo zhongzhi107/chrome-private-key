02bd9be7da0222a9e919a9415e163aee85fc6af9
zhi.zhong <zhi.zhong@163.com>
2015-12-01 14:33:59 +0800

    Update CSS Selector

--- content_script.js
+++ content_script.js
@@ -1,12 +1,4 @@
 'use strict';
 
-// BDS和QDR上tagname文本框的位置不一样
-var tagnameIndex = 0;
-if (location.hostname.indexOf('bds.') === 0) {
-  tagnameIndex = 53;
-} else if (location.hostname.indexOf('qdr.') === 0) {
-  tagnameIndex = 8;
-}
-
 document.querySelector('input[type=password]').value = 'xgz7h.l6mup_f%_';
-document.querySelectorAll('.setting-input')[tagnameIndex].value = '其他';
+document.querySelector('input[value="tag_message"]+input').value = '其他';
